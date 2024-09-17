e1a2653ed8c4d5f6d25dee0832308da170b25af5
IT24101563 <it24101563@my.sliit.lk>
2024-09-17 23:22:37 +0530

    Update IT24101563Lab6Q2B.java

--- Q2/IT24101563Lab6Q2B.java
+++ Q2/IT24101563Lab6Q2B.java
@@ -5,19 +5,18 @@ public class IT24101003Lab6Q2B{
 	public static void main(String []args){
 		
 		Scanner scanner = new Scanner(System.in);
-		String output="";
+		String result="";
 		int count =1;
 		System.out.println("Please enter 10 numbers");
 		
 		while(count <= 10){
 		System.out.print("Enter number" + count+": ");
 		int input = scanner.nextInt();
-		output += input +" "; 
+		result += input +" "; 
 		count++;
 		}
 		
-		System.out.print(output);
-		scanner.close();
+		System.out.print(result);
 		
 		
 	}
